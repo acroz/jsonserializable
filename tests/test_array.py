6de652f85da7e41b271260a2a3e423ae520b2b55
Andrew Crozier <wacrozier@gmail.com>
2017-05-28 21:49:28 +0100

    Added additional tests for arrays

--- tests/test_array.py
+++ tests/test_array.py
@@ -1,15 +1,37 @@
-import jsonserializable
 import pytest
+from jsonschema import ValidationError
+import jsonserializable
 
 
 IntArray = jsonserializable.Array[int]
 
 
-def test_serialize_array():
-    array = IntArray([1, 2, 3])
-    assert array.serialize() == [1, 2, 3]
+@pytest.mark.parametrize('data', [[], [1, 2, 3]])
+def test_serialize_array(data):
+    array = IntArray(data)
+    assert array.serialize() == data
 
 
-def test_deserialize_array():
-    array = IntArray.deserialize([1, 2, 3])
-    assert array == IntArray([1, 2, 3])
+@pytest.mark.parametrize('data', [[], [1, 2, 3]])
+def test_deserialize_array(data):
+    array = IntArray.deserialize(data)
+    assert array == IntArray(data)
+
+
+def test_schema():
+    assert IntArray.schema() == {
+        'type': 'array',
+        'items': {'type': 'number'}
+    }
+
+
+@pytest.mark.parametrize('data', [['foo'], [1, 2, 'bar']])
+def test_invalid_type(data):
+    with pytest.raises(TypeError):
+        IntArray(data)
+
+
+@pytest.mark.parametrize('data', [['foo'], [1, 2, 'bar']])
+def test_invalid_schema(data):
+    with pytest.raises(ValidationError):
+        IntArray.deserialize(data)
