a85d15c2167a3e59b7ba29e5bcf71f4794dbc2a6
Andrew Crozier <wacrozier@gmail.com>
2017-05-28 21:59:58 +0100

    Added tests for Mapping type

---
+++ tests/test_mapping.py
@@ -0,0 +1,34 @@
+import pytest
+from jsonschema import ValidationError
+from jsonserializable import Mapping
+
+
+@pytest.mark.parametrize('data', [{}, {'one': 1, 'two': 2}])
+def test_serialize_mapping(data):
+    mapping = Mapping[int](data)
+    assert mapping.serialize() == data
+
+
+@pytest.mark.parametrize('data', [{}, {'one': 1, 'two': 2}])
+def test_deserialize_mapping(data):
+    mapping = Mapping[int].deserialize(data)
+    assert mapping == Mapping[int](data)
+
+
+def test_schema():
+    assert Mapping[int].schema() == {
+        'type': 'object',
+        'additionalProperties': {'type': 'number'}
+    }
+
+
+@pytest.mark.parametrize('data', [{'one': 'foo'}, {'one': 1, 'two': 'bar'}])
+def test_invalid_type(data):
+    with pytest.raises(TypeError):
+        Mapping[int](data)
+
+
+@pytest.mark.parametrize('data', [{'one': 'foo'}, {'one': 1, 'two': 'bar'}])
+def test_invalid_schema(data):
+    with pytest.raises(ValidationError):
+        Mapping[int].deserialize(data)
