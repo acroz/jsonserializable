5b749736be595364ad8e9e3af1798a57d99ce97f
Andrew Crozier <wacrozier@gmail.com>
2017-05-29 02:08:54 +0100

    Added tests for isinstance/issubclass on array

--- tests/test_array.py
+++ tests/test_array.py
@@ -65,3 +65,27 @@ def test_insert_invalid_type(data):
     array = Array[int]([1, 2, 3])
     with pytest.raises(TypeError):
         array.insert(1, data)
+
+
+class IntArray(Array[int]):  # type: ignore
+    pass
+
+
+@pytest.mark.parametrize('value', [Array[int]([1, 2]), IntArray([1, 2])])
+def test_isinstance_true(value):
+    assert isinstance(value, Array)
+    assert isinstance(value, Array[int])
+
+
+def test_isinstance_false():
+    assert not isinstance(Array[int]([1, 2]), IntArray)
+
+
+@pytest.mark.parametrize('cls', [Array[int], IntArray])
+def test_issubclass_true(cls):
+    assert issubclass(cls, Array)
+    assert issubclass(cls, Array[int])
+
+
+def test_issubclass_false():
+    assert not issubclass(Array[int], IntArray)
