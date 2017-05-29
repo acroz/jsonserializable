e5d39c4a18c8a2699acf1858bc7836b47797d4f4
Andrew Crozier <wacrozier@gmail.com>
2017-05-29 18:05:46 +0100

    Added tests on enum usage

--- tests/test_enum.py
+++ tests/test_enum.py
@@ -4,16 +4,54 @@ from jsonserializable import Enum, List
 
 
 class ExampleEnum(Enum):
-    foo = 'foo'
+    foo = 'bar'
     one = 1
     array = List[int]([1, 2])
 
 
 EXAMPLE_SERIALIZATIONS = [
-    (ExampleEnum.foo, 'foo'),
+    (ExampleEnum.foo, 'bar'),
     (ExampleEnum.one, 1),
     (ExampleEnum.array, List[int]([1, 2]).serialize())
 ]
+
+
+def test_getattr():
+    member = ExampleEnum.foo
+    assert member.name == 'foo'
+    assert member.value == 'bar'
+
+
+def test_getattr_missing():
+    with pytest.raises(AttributeError):
+        ExampleEnum.other
+
+
+def test_getitem():
+    assert ExampleEnum['foo'] == ExampleEnum.foo
+
+
+def test_getitem_missing():
+    with pytest.raises(KeyError):
+        ExampleEnum['other']
+
+
+def test_fromvalue():
+    assert ExampleEnum.from_value('bar') == ExampleEnum.foo
+
+
+def test_fromvalue_unhashable():
+    assert ExampleEnum.from_value(List[int]([1, 2])) == ExampleEnum.array
+
+
+def test_fromvalue_missing():
+    with pytest.raises(ValueError):
+        ExampleEnum.from_value('other')
+
+
+def test_fromvalue_missing_unhashable():
+    with pytest.raises(ValueError):
+        ExampleEnum.from_value(List[str](['foo', 'bar']))
 
 
 def test_serialize_enum():
@@ -37,8 +75,8 @@ def test_deserialize_member(member, data):
 
 
 def test_schema_enum():
-    assert ExampleEnum.schema() == {'enum': ['foo', 1, [1, 2]]}
+    assert ExampleEnum.schema() == {'enum': ['bar', 1, [1, 2]]}
 
 
 def test_schema_member():
-    assert ExampleEnum.foo.schema() == {'enum': ['foo', 1,[1, 2]]}
+    assert ExampleEnum.foo.schema() == {'enum': ['bar', 1,[1, 2]]}
